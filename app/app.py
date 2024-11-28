50455c3f0cf4468872ba8522f84b6ef71b91f128
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 13:26:33 +0200

    change the url_prefix to '/api'

--- app/app.py
+++ app/app.py
@@ -4,9 +4,9 @@ import logging
 
 app = Flask(__name__)
 
-app.register_blueprint(phone_bp, url_prefix='/api/phone_tracker')
+app.register_blueprint(phone_bp, url_prefix='/api')
 
-app.config['DEBUG'] = True
+app.config['DEBUG'] = Truech
 app.config['LOGGING_LEVEL'] = logging.DEBUG
 
 @app.route('/', methods=['GET'])
