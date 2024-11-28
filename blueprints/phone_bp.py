887a9211699e47d2946fda19c8135d7d10e531d5
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 11:21:27 +0200

    create blueprints folder with phone_dp

---
+++ blueprints/phone_bp.py
@@ -0,0 +1,10 @@
+from flask import Blueprint, request, jsonify, current_app
+import json
+import logging
+
+phone_bp = Blueprint('phone_bp', __name__)
+
+@phone_bp.route("/api/phone_tracker", methods=['POST'])
+def get_interaction():
+   print(request.json)
+   return jsonify({ }), 200
