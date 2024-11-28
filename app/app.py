9acefc2610436aa5ba8050cb8c30d6142d3d4f00
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 13:32:19 +0200

    fix a spelling mistake

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 app.register_blueprint(phone_bp, url_prefix='/api')
 
-app.config['DEBUG'] = Truech
+app.config['DEBUG'] = True
 app.config['LOGGING_LEVEL'] = logging.DEBUG
 
 @app.route('/', methods=['GET'])
