2237375079e7f3de093c3331ede5d96148062ade
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 13:29:20 +0200

    added code to connect with a service that will insert the data into neo4j database

--- app/blueprints/phone_bp.py
+++ app/blueprints/phone_bp.py
@@ -1,8 +1,29 @@
 from flask import Blueprint, request, jsonify, current_app
+from app.services.phone_service import PhoneRepository
+import logging
 
 phone_bp = Blueprint('phone_bp', __name__)
 
-@phone_bp.route("/api/phone_tracker", methods=['POST'])
+@phone_bp.route("/phone_tracker", methods=['POST'])
 def get_interaction():
-   print(request.json)
-   return jsonify({ }), 200
+   data = request.get_json()
+   try:
+      repo = PhoneRepository(current_app.neo4j_driver)
+      interaction = repo.create_phone_interaction(data)
+
+      return jsonify({
+         'status': 'success',
+         'interaction': interaction
+      }), 201
+
+   except Exception as e:
+      print(f'Error in POST /api/phone_tracker: {str(e)}')
+      logging.error(f'Error in POST /api/phone_tracker: {str(e)}')
+      return jsonify({'error': 'internal server error'}), 500
+
+
+
+
+
+
+
