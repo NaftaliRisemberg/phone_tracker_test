9a9337434976eb3a8405b901b074429e2394d079
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 11:32:44 +0200

    added the register blueprint, added logging config

--- app/app.py
+++ app/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, jsonify
+from blueprints import phone_bp
+import logging
 
 app = Flask(__name__)
+
+app.register_blueprint(phone_bp, url_prefix='/api/phone_tracker')
+
+app.config['DEBUG'] = True
+app.config['LOGGING_LEVEL'] = logging.DEBUG
 
 @app.route('/', methods=['GET'])
 def work_check():
