5709f66f674a05e262ec6b29f962207a982f6083
NaftaliRisemberg <n9742686@gmail.com>
2024-11-28 13:40:15 +0200

    added app_context

--- app/app.py
+++ app/app.py
@@ -1,13 +1,17 @@
 from flask import Flask, jsonify
+from init_db import init_neo4j
 from blueprints import phone_bp
 import logging
 
 app = Flask(__name__)
 
-app.register_blueprint(phone_bp, url_prefix='/api')
-
 app.config['DEBUG'] = True
 app.config['LOGGING_LEVEL'] = logging.DEBUG
+
+app.register_blueprint(phone_bp, url_prefix='/api')
+
+with app.app_context():
+    app.neo4j_driver = init_neo4j()
 
 @app.route('/', methods=['GET'])
 def work_check():
