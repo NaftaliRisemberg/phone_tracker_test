0d39466a3e9dc4ac08601677f217bd563aaff361
NaftaliRisemberg <n9742686@gmail.com>
2024-12-01 09:58:04 +0200

    added a find_count_by_sender_id function

--- blueprints/phone_retrieval_bp.py
+++ blueprints/phone_retrieval_bp.py
@@ -30,3 +30,21 @@ def find_stronger_interaction():
       print(f'Error in GET /api/find_stronger_than_-60: {str(e)}')
       logging.error(f'Error in GET /api/find_stronger_than_-60: {str(e)}')
       return jsonify({'error': 'internal server error'}), 500
+
+@get_phone_bp.route('/find_count_by_sender_id', methods=['GET'])
+def find_count_by_sender_id():
+   try:
+      sender_id = request.args.get('id')
+
+      if sender_id is None:
+         return jsonify({'error': 'Missing sender_id parameter'}), 400
+
+      repo = GetPhoneRepository(current_app.neo4j_driver)
+      data = repo.find_count_by_sender_id()
+
+      return jsonify(data), 200
+
+   except Exception as e:
+      print(f'Error in GET /api/find_stronger_than_-60: {str(e)}')
+      logging.error(f'Error in GET /api/find_stronger_than_-60: {str(e)}')
+      return jsonify({'error': 'internal server error'}), 500
